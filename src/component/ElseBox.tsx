7ecb3a038c44c5b474b28f5c94e38d6db7d830ce
lee gueyeol <i1004gy@gmail.com>
2024-06-10 11:45:34 +0900

    [#5] feat: 박스 디자인 하이라이트 구현

--- src/component/ElseBox.tsx
+++ src/component/ElseBox.tsx
@@ -2,11 +2,11 @@
 import { ReactNode } from "react";
 import styles from "./ForBox.module.css";
 import cx from "classnames";
-type Props = { children?: ReactNode; animation?: boolean };
-function IfBox({ children, animation }: Props) {
+type Props = { children?: ReactNode; lightOn: boolean };
+function IfBox({ children, lightOn }: Props) {
   return (
-    <div className={cx(styles.for_box, animation && styles.box_animation)}>
-      <div className={styles.for_border}>
+    <div className={styles.for_box}>
+      <div className={cx(styles.for_border, lightOn && styles.highlight)}>
         <span className={styles.for_text}>else</span>
         {children && <div>{children}</div>}
       </div>
