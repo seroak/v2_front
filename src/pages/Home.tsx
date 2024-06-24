ecd5ff2234482d0b382c71ec00ead6ce9f3b2939
lee gyuyeol <i1004gy@gmail.com>
2024-06-24 18:21:18 +0900

    [#34]feat: dummyItem 부분 타입에러 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,40 @@ import RightSection from "../component/RightSection";
 import Resizable from "../component/Resizable";
 import { createContext, useState, Dispatch, SetStateAction } from "react";
 
+interface DummyItem {
+  id?: number;
+  type: string;
+  depth?: number;
+  value?: number;
+  name?: string;
+  start?: number;
+  end?: number;
+  cur?: number;
+  expr?: string;
+  highlight?: number[] | string[];
+  condition?: ConditionItem;
+  variables?: VariableList[];
+}
+interface VarItem extends DummyItem {
+  lightOn?: boolean;
+}
+interface VariableList {
+  name: string;
+  expr: string;
+  depth: number;
+  type?: string;
+}
+interface ConditionItem {
+  target: string;
+  start: number;
+  end: number;
+  cur: number;
+  step: number;
+}
 // context 타입 정의
 interface CodeContextType {
-  codeData: object;
-  setCodeData: Dispatch<SetStateAction<object>>;
+  codeData: DummyItem[];
+  setCodeData: Dispatch<SetStateAction<DummyItem[]>>;
 }
 export const CodeContext = createContext<CodeContextType>({
   codeData: {},
