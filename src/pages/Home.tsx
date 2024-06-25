eed714add087f0d2d4e86a9c99b17833072efed6
lee gyuyeol <i1004gy@gmail.com>
2024-06-25 10:29:39 +0900

    [#34]feat: header 생성 및 home에 form태그 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import style from "./home.module.css";
+import styles from "./home.module.css";
 import CodeEditor from "../component/CodeEditor";
 import RightSection from "../component/RightSection";
 import Resizable from "../component/Resizable";
@@ -45,11 +45,22 @@ export const CodeContext = createContext<CodeContextType>({
 });
 export default function Home() {
   const [codeData, setCodeData] = useState({});
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // editorData를 사용하여 제출 로직 처리
+    console.log(codeData);
+  };
   return (
     <CodeContext.Provider value={{ codeData, setCodeData }}>
-      <main className={style.main}>
-        <h1>Haeya 에디터</h1>
-        <Resizable left={<CodeEditor />} right={<RightSection />} />
+      <main className={styles.main}>
+        <form onSubmit={handleSubmit}>
+          <div className={styles.header}>
+            <button type="submit" className={styles.button}>
+              Run
+            </button>
+          </div>
+          <Resizable left={<CodeEditor />} right={<RightSection />} />
+        </form>
       </main>
     </CodeContext.Provider>
   );
