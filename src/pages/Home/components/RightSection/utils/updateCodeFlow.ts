69c5b6d76ff855979072d7a0600f0da1a2aed61a
lee gyuyeol <i1004gy@gmail.com>
2024-07-09 17:10:13 +0900

    [#41]feat: 주석 변경

--- src/pages/Home/components/RightSection/utils/updateCodeFlow.ts
+++ src/pages/Home/components/RightSection/utils/updateCodeFlow.ts
@@ -3,22 +3,23 @@ import { AllObjectItem } from "@/types/allObjectItem";
 // 객체를 수정해야하는 경우에 실행하는 함수
 export const updateCodeFlow = (
   codeFlows: AllObjectItem[], //현제 코드흐름 시각화 정보를 담고 있는 리스트
-  newObject: AllObjectItem //수정해야하는 정보를 담고 있는 객체
+  toAddObject: AllObjectItem //수정해야하는 정보를 담고 있는 객체
 ): AllObjectItem[] => {
+  // 발견하고 반복문이 바로 끝나는 것이 아니라 계속 돌면서 남아있는 객체도 반환해야한다
   return codeFlows.map((codeFlow) => {
-    // 비주얼 스택을 순회하면서 수정해야하는 위치를 찾는다
-    // 발견하고 반복문이 바로 끝나는 것이 아니라 계속 돌면서 남아있는 객체도 반환해야한다
-    if (codeFlow.id === newObject.id) {
-      // 발견하면 들어가서 수정한다
-      return { ...codeFlow, ...newObject, child: codeFlow.child };
-    } else if (codeFlow.child && codeFlow.child.length > 0) {
-      // 끝까지 찾아도 없으면 child로 들어가서 재귀적으로 탐색한다
+    // 맞는 id를 발견하면 들어가서 수정한다
+    if (codeFlow.id === toAddObject.id) {
+      return { ...codeFlow, ...toAddObject, child: codeFlow.child };
+    }
+    // 끝까지 찾아도 없으면 child로 들어가서 재귀적으로 탐색한다
+    else if (codeFlow.child && codeFlow.child.length > 0) {
       return {
         ...codeFlow,
-        child: updateCodeFlow(codeFlow.child, newObject),
+        child: updateCodeFlow(codeFlow.child, toAddObject),
       };
-    } else {
-      // 끝까지 찾아도 없으면 그냥 객체를 반환한다
+    }
+    // 끝까지 찾아도 없으면 그냥 객체를 반환한다
+    else {
       return codeFlow;
     }
   });
