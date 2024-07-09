0e737206ace8430ac82de177dce80016de324633
lee gyuyeol <i1004gy@gmail.com>
2024-07-09 15:53:08 +0900

    [#41]feat: jest 환경설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
+import path from "path";
 // https://vitejs.dev/config/
 export default ({ mode }: { mode: string }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return defineConfig({
     plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
     server: {
       port: env.VITE_APP_FRONT_PORT === "dev" ? 8000 : 5000,
     },
