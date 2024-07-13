5d5a1e29fd03dc3c69fa05f2884468b9cf000e6e
lee gyuyeol <i1004gy@gmail.com>
2024-07-13 10:50:21 +0900

    [#50]feat: ifelse용 테스트 api생성

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,14 +1,20 @@
-// src/mocks/handlers.js
 import { http, HttpResponse } from "msw";
-import forPrintMockData from "@/fixtures/forPrintMockData.json";
+// import successForAndPrintResponseBody from "./samples/successForAndPrintResponseBody.json";
+import successIfAndElseResponseBody from "./samples/successIfAndElseResponseBody.json";
 // export const handlers = [
 //   http.post("/v1/python", () => {
 //     return HttpResponse.json([{ code: "test" }]);
 //   }),
 // ];
 
+// export const handlers = [
+//   http.post("/v1/python", () => {
+//     return HttpResponse.json(successForAndPrintResponseBody);
+//   }),
+// ];
+
 export const handlers = [
   http.post("/v1/python", () => {
-    return HttpResponse.json(forPrintMockData);
+    return HttpResponse.json(successIfAndElseResponseBody);
   }),
 ];
