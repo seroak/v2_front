d05e49aff7a541c91b118b2be649b8605f9fd65b
lee gyu yeol-mac <i1004gy@gmail.com>
2024-07-18 15:12:03 +0900

    [#50]feat: prettier 속성 변경으로 인한 변경사항

--- src/pages/Home/components/RightSection/components/ElseBox/ElseBox.tsx
+++ src/pages/Home/components/RightSection/components/ElseBox/ElseBox.tsx
@@ -1,8 +1,8 @@
-import { ReactNode } from "react";
-import styles from "./ElseBox.module.css";
-import cx from "classnames";
-import { AnimatePresence, motion } from "framer-motion";
-import { ConditionItem } from "@/pages/Home/types/conditionItem";
+import { ReactNode } from 'react';
+import styles from './ElseBox.module.css';
+import cx from 'classnames';
+import { AnimatePresence, motion } from 'framer-motion';
+import { ConditionItem } from '@/pages/Home/types/conditionItem';
 type Props = {
   children?: ReactNode;
   isLight: boolean;
@@ -25,19 +25,13 @@ function ElseBox({ children, isLight, elseItem }: Props) {
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.3 }}
-          className={cx(
-            styles.else_border,
-            elseItem.isLight && styles.highlight
-          )}
+          className={cx(styles.else_border, elseItem.isLight && styles.highlight)}
         >
           <motion.div layout className={styles.else_title}>
             <span>else</span>
           </motion.div>
-          {isLight && elseItem.expr === "True" ? (
-            <motion.div
-              layout
-              className={cx(styles.else_expr, isLight && styles.highlight)}
-            >
+          {isLight && elseItem.expr === 'True' ? (
+            <motion.div layout className={cx(styles.else_expr, isLight && styles.highlight)}>
               <span>True</span>
             </motion.div>
           ) : null}
