2730af15f4526e973101e8b2e430922be2f33026
lee gyu yeol-mac <i1004gy@gmail.com>
2024-07-23 11:35:17 +0900

    [#21]feat: 테스트 api

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,7 @@
-import { http, HttpResponse } from 'msw';
-import successForAndPrintResponseBody from './samples/successForAndPrintResponseBody.json';
-import successIfAndElseResponseBody from './samples/successIfAndElseResponseBody.json';
-import successAssignResponseBody from './samples/successAssignResponseBody.json';
+import { http, HttpResponse } from "msw";
+import successForAndPrintResponseBody from "./samples/successForAndPrintResponseBody.json";
+import successIfAndElseResponseBody from "./samples/successIfAndElseResponseBody.json";
+import successAssignResponseBody from "./samples/successAssignResponseBody.json";
 // export const handlers = [
 //   http.post("/v1/python", () => {
 //     return HttpResponse.json([{ code: "test" }]);
@@ -9,11 +9,11 @@ import successAssignResponseBody from './samples/successAssignResponseBody.json'
 // ];
 
 // for, print 데이터 테스트
-// export const handlers = [
-//   http.post("/v1/python", () => {
-//     return HttpResponse.json(successForAndPrintResponseBody);
-//   }),
-// ];
+export const handlers = [
+  http.post("/v1/python", () => {
+    return HttpResponse.json(successForAndPrintResponseBody);
+  }),
+];
 
 // if, elif, else 데이터 테스트
 // export const handlers = [
@@ -22,8 +22,8 @@ import successAssignResponseBody from './samples/successAssignResponseBody.json'
 //     }),
 // ];
 
-export const handlers = [
-  http.post('/v1/python', () => {
-    return HttpResponse.json(successAssignResponseBody);
-  }),
-];
+// export const handlers = [
+//   http.post('/v1/python', () => {
+//     return HttpResponse.json(successAssignResponseBody);
+//   }),
+// ];
