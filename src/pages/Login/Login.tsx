01bf4ecd0594fa34417937b61e528cbd3c1f2985
lee gyu yeol-mac <i1004gy@gmail.com>
2024-07-25 18:42:40 +0900

    [#64]feat: 로그인 리액트쿼리로 백엔드에 전송하는 api코드

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,13 +1,38 @@
 import { useState, FormEvent, ChangeEvent } from "react";
 import styles from "./Login.module.css";
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
+import { useUserStore } from "@/store/user";
 
 const Login = () => {
   const [userId, setUserId] = useState<string>("");
   const [userPassword, setUserPassword] = useState<string>("");
+  const setLogedInUserId = useUserStore((state) => state.setLogedInUserId);
+  const setLogedInUserPassword = useUserStore((state) => state.setLogedInUserPassword);
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const mutation = useMutation({
+    mutationFn: async ({ userId, userPassword }: { userId: string; userPassword: string }) => {
+      return axios.post(
+        "http://localhost:8000/login",
+        { userId, userPassword },
+        { headers: { "Content-Type": "application/json" } }
+      );
+    },
+    onSuccess(data) {
+      const jsonData = data.data;
+      console.log("Login success:", jsonData);
+      setLogedInUserId(jsonData.user.id);
+      setLogedInUserPassword(jsonData.user.name);
+    },
+    onError(error) {
+      console.error("Login error:", error);
+    },
+  });
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // 여기에 로그인 로직을 추가하세요
+    mutation.mutate({ userId, userPassword });
     console.log("Login attempt:", { userId, userPassword });
   };
   const handleUserIdChange = (e: ChangeEvent<HTMLInputElement>) => {
