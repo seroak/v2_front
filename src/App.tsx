bc936dd18ade74560c61c1adcc6505096aa7ca2b
lee gyu yeol-mac <i1004gy@gmail.com>
2024-07-26 12:52:56 +0900

    [#74]Feat: 로그인 라우팅 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import "./App.css";
 
@@ -17,7 +16,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
