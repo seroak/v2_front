53e55e21aa4865a9e9feee7b77d0ee1c9a2be222
lee gyuyeol <i1004gy@gmail.com>
2024-07-28 18:11:52 +0900

    [#67]feat: 회원가입 용 mock api코드

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,29 +1,24 @@
 import { http, HttpResponse } from "msw";
 import successForAndPrintResponseBody from "./samples/successForAndPrintResponseBody.json";
-import successIfAndElseResponseBody from "./samples/successIfAndElseResponseBody.json";
-import successAssignResponseBody from "./samples/successAssignResponseBody.json";
-// export const handlers = [
-//   http.post("/v1/python", () => {
-//     return HttpResponse.json([{ code: "test" }]);
-//   }),
-// ];
-
-// for, print 데이터 테스트
+interface SignupUser {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 export const handlers = [
   http.post("/v1/python", () => {
     return HttpResponse.json(successForAndPrintResponseBody);
   }),
+  http.post("/signup", async ({ request }) => {
+    const { username } = (await request.json()) as SignupUser;
+    if (username === "error") return HttpResponse.json({ message: "error" }, { status: 500 });
+    return HttpResponse.json(
+      {
+        success: true,
+        message: "로그인 성공",
+      },
+      { status: 200 }
+    );
+  }),
 ];
-
-// if, elif, else 데이터 테스트
-// export const handlers = [
-//     http.post('/v1/python', () => {
-//         return HttpResponse.json(successIfAndElseResponseBody);
-//     }),
-// ];
-
-// export const handlers = [
-//   http.post("/v1/python", () => {
-//     return HttpResponse.json(successAssignResponseBody);
-//   }),
-// ];
