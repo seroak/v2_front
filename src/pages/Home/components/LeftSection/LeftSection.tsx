96ee415a3defcdc03047fa0356f0c5af0f64c77b
lee gyu yeol-mac <i1004gy@gmail.com>
2024-07-30 15:12:28 +0900

    [#81]feat: leftSection 위아래로 나누기

--- src/pages/Home/components/LeftSection/LeftSection.tsx
+++ src/pages/Home/components/LeftSection/LeftSection.tsx
@@ -1,12 +1,31 @@
+import { Fragment } from "react/jsx-runtime";
+import styles from "./LeftSection.module.css";
 import CodeEditor from "./components/CodeEditor";
 import Console from "./components/Console";
-
+import Split from "react-split";
 const LeftSection = () => {
   return (
-    <div style={{ height: "100vh", width: "100%", overflow: "scroll" }}>
-      <CodeEditor />
-      <Console />
-    </div>
+    <Fragment>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <p className={styles["view-section-title"]}>코드작성</p>
+        <Split
+          sizes={[50, 50]}
+          minSize={100}
+          expandToMin={false}
+          gutterSize={10}
+          gutterAlign="center"
+          snapOffset={30}
+          dragInterval={1}
+          direction="vertical"
+          cursor="row-resize"
+          style={{ display: "flex", flexDirection: "column", height: "100vh" }}
+          className={styles.splitContainer}
+        >
+          <CodeEditor />
+          <Console />
+        </Split>
+      </div>
+    </Fragment>
   );
 };
 
