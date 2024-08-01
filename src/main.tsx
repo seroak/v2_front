e8b8f856c7e8cd742b6d242b37486f013fc4e2d7
lee gyu yeol-mac <i1004gy@gmail.com>
2024-08-01 20:32:24 +0900

    [#83]feat: 주석 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+
 import RQProvider from "./RQProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  //시작
-
   <RQProvider>
     <App />
   </RQProvider>
