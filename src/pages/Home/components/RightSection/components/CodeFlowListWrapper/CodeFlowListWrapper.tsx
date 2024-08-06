4385a7d3cbd19dc7ae89f4efd01b1f2eddf414a1
lee gyu yeol-mac <i1004gy@gmail.com>
2024-08-06 22:11:15 +0900

    [#72]feat: 코드흐름에서 list를 감싸는 컴포넌트

---
+++ src/pages/Home/components/RightSection/components/CodeFlowListWrapper/CodeFlowListWrapper.tsx
@@ -0,0 +1,23 @@
+import styles from "./ListWrapper.module.css";
+import { ListBlock } from "./components/CodeFlowListBlock";
+import { DataStructureListItem } from "@/pages/Home/types/dataStructureListItem";
+type Props = {
+  listItem: DataStructureListItem;
+};
+function ListWrapper({ listItem }: Props) {
+  const { expr, isLight, name } = listItem;
+  const exprArray = expr?.slice(1, -1).split(",");
+
+  return (
+    <div>
+      <span>{name}</span>
+      <div className={styles.wrapper}>
+        {exprArray?.map((exprItem, index) => {
+          return <ListBlock key={index} exprItem={exprItem} isLight={isLight} index={index} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default ListWrapper;
