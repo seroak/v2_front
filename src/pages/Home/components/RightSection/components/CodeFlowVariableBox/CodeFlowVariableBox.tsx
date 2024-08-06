fdbe37a38645e1492b7916da3f4e82e73ee7c931
lee gyu yeol-mac <i1004gy@gmail.com>
2024-08-06 14:53:08 +0900

    [#97]feat: 변수 시각화의 위치를 알기위한 코드

--- src/pages/Home/components/RightSection/components/CodeFlowVariableBox/CodeFlowVariableBox.tsx
+++ src/pages/Home/components/RightSection/components/CodeFlowVariableBox/CodeFlowVariableBox.tsx
@@ -1,17 +1,54 @@
+import { useRef, useEffect, ReactNode } from "react";
 import styles from "./CodeFlowVariableBox.module.css";
 import cx from "classnames";
+
+//type
 import { CodeFlowVariableItem } from "@/pages/Home/types/codeFlow/codeFlowVariableItem";
+
+//zustand
+import { useArrowStore } from "@/store/arrow";
+interface CodeFlowVariableItemProps {
+  codeFlowVariableItem: CodeFlowVariableItem;
+  isTracking: boolean;
+  children?: ReactNode;
+}
+const GetCodeFlowVariableBoxLocation = ({ codeFlowVariableItem, isTracking, children }: CodeFlowVariableItemProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const setTop = useArrowStore((state) => state.setTop);
+  const setRight = useArrowStore((state) => state.setRight);
+
+  useEffect(() => {
+    if (ref.current && isTracking) {
+      const rect = ref.current.getBoundingClientRect();
+      setTop(rect.top - 40);
+      setRight(rect.right + 10);
+    }
+  }, [codeFlowVariableItem.id, codeFlowVariableItem.type, isTracking]);
+
+  return (
+    <div className="useRef" ref={ref}>
+      {children}
+    </div>
+  );
+};
 type Props = {
   codeFlowVariableItem: CodeFlowVariableItem;
+  isTracking: boolean;
 };
-function CodeFlowVariableBox({ codeFlowVariableItem }: Props) {
+const CodeFlowVariableBox = ({ codeFlowVariableItem, isTracking }: Props) => {
   return (
     <div className={styles.variable}>
       <div className={cx(styles.variable_box, codeFlowVariableItem.isLight && styles.highlight)}>
-        <span className={styles.text}>{codeFlowVariableItem.expr}</span>
+        <GetCodeFlowVariableBoxLocation
+          key={codeFlowVariableItem.id}
+          codeFlowVariableItem={codeFlowVariableItem}
+          isTracking={isTracking}
+        >
+          <span className={styles.text}>{codeFlowVariableItem.expr}</span>
+        </GetCodeFlowVariableBoxLocation>
       </div>
     </div>
   );
-}
+};
 
 export default CodeFlowVariableBox;
