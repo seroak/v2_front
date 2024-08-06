4523a2ddbf2c2f7943e0179c0969c63761033975
lee gyu yeol-mac <i1004gy@gmail.com>
2024-08-06 14:57:56 +0900

    [#97]feat: while 컴포넌트

---
+++ src/pages/Home/components/RightSection/components/WhileBox/WhileBox.tsx
@@ -0,0 +1,54 @@
+import { ReactNode } from "react";
+import cx from "classnames";
+import { WhileItem } from "@/pages/Home/types/whileItem";
+import { AnimatePresence, motion } from "framer-motion";
+
+type Props = {
+  children: ReactNode;
+  whileItem: WhileItem;
+};
+
+const WhileBox = ({ children, whileItem }: Props) => {
+  return (
+    <AnimatePresence key={whileItem.id}>
+      <motion.div
+        layout
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.3 }}
+        className={cx("code-flow", "code-flow-While", whileItem.isLight && "highlight-border")}
+      >
+        <motion.div
+          layout
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          className={cx("code-flow-title-wrap", whileItem.isLight && "highlight")}
+        >
+          <motion.div className="code-flow-title">
+            <span>while</span>
+          </motion.div>
+          <div className="code-flow-var">
+            <div>
+              <span
+                className={cx(
+                  whileItem.isLight && "highlight-number",
+                  whileItem.isLight && whileItem.expr === "False" && "false"
+                )}
+              >
+                {whileItem.expr?.split("").map((char, index) => (
+                  <text key={index}>{char}</text>
+                ))}
+              </span>
+            </div>
+          </div>
+        </motion.div>
+        {children && <div>{children}</div>}
+      </motion.div>
+    </AnimatePresence>
+  );
+};
+
+export default WhileBox;
