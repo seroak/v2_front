91eb04da433b5f7c547d4d5c47e31c0aab12ed2c
lee gyuyeol <i1004gy@gmail.com>
2024-08-14 15:23:02 +0900

    [#118]fix: 더 깊은 depth로 추가하는 함수

---
+++ src/pages/Home/components/RightSection/services/insertIntoDepth.ts
@@ -0,0 +1,23 @@
+import { AllObjectItem } from "@/pages/Home/types/allObjectItem";
+export const insertIntoDepth = (
+  codeFlows: AllObjectItem[],
+  toAddObject: AllObjectItem,
+  prevTrackingId: number
+): AllObjectItem[] => {
+  return codeFlows.map((codeFlow) => {
+    console.log(prevTrackingId);
+    if (codeFlow.id === prevTrackingId) {
+      return {
+        ...codeFlow,
+        child: [toAddObject, ...codeFlow.child],
+      };
+    } else if (codeFlow.child && codeFlow.child.length > 0) {
+      return {
+        ...codeFlow,
+        child: insertIntoDepth(codeFlow.child, toAddObject, prevTrackingId),
+      };
+    } else {
+      return codeFlow;
+    }
+  });
+};
