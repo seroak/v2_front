079ca8b5ed5888d58b2572d3634ea8a262b655b9
lee gyuyeol <i1004gy@gmail.com>
2024-09-02 18:00:47 +0900

    [#131]test: 주석 변경

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-    "^monaco-editor$": "<rootDir>/__mocks__/monacoEditorMock.ts", // 가짜 모듈을 사용
+    "^monaco-editor$": "<rootDir>/__mocks__/monacoEditorMock.ts", // monaco-editor 목업 사용
   },
   transform: {
     "^.+\\.(ts|tsx)?$": "ts-jest",
