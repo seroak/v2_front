f800290ce105029578669bf86d8c512c863030e5
lee gyu yeol-mac <i1004gy@gmail.com>
2024-09-04 22:10:01 +0900

    [#136]feat: 버튼 작동되게 변경

--- src/pages/components/PublicHeader.tsx
+++ src/pages/components/PublicHeader.tsx
@@ -5,15 +5,16 @@ const PublicHeader = () => {
   return (
     <header className={styles["bg-blue"]}>
       <div className={styles["header-menu"]}>
-        <a className={styles["header-logo"]} href="">
+        <Link className={styles["header-logo"]} to="/">
           <img src="/image/img_logo.png" alt="로고" />
-        </a>
+        </Link>
         {/* <!-- 활성화 할 a태그에 on_active 클래스 추가 --> */}
 
         <Link to="/viz">시각화</Link>
 
         <a href="#">가격</a>
       </div>
+
       <div>
         <Link to="/login" className={styles["login-btn"]}>
           로그인
