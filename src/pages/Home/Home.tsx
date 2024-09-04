c2550555ecb01d3b7ee1042c371bc4926e94dc79
lee gyu yeol-mac <i1004gy@gmail.com>
2024-09-04 22:10:52 +0900

    [#136]feat: 메인 페이지 임시로 구현

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import PublicHeader from "../components/PublicHeader";
+import LoggedInHeader from "../components/LoggedInHeader";
+import { useUserStore } from "@/store/user";
+const Home = () => {
+  const loggedInUserName = useUserStore((state) => state.loggedInUserName);
+  return (
+    <>
+      {loggedInUserName === "" ? <PublicHeader /> : <LoggedInHeader />}
+      <h1>메인페이지 입니다</h1>
+    </>
+  );
+};
+export default Home;
