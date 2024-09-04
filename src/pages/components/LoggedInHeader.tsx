ca7861c9b6e28da58e07296bc689d12ce4c9ef6c
lee gyu yeol-mac <i1004gy@gmail.com>
2024-09-04 22:09:21 +0900

    [#136]feat: 로그인 할때 사용자 이름 띄어주고 로그아웃 기능 구현

--- src/pages/components/LoggedInHeader.tsx
+++ src/pages/components/LoggedInHeader.tsx
@@ -1,25 +1,35 @@
-import { Link } from "react-router-dom";
 import styles from "./LoggedInHeader.module.css";
-
+import { useUserStore } from "@/store/user";
+import { Cookies } from "react-cookie";
+import { Link } from "react-router-dom";
 const LoggedInHeader = () => {
+  const cookies = new Cookies();
+  const loggedInUserName = useUserStore((state) => state.loggedInUserName);
+  const resetUser = useUserStore((state) => state.resetUser);
+  const logout = () => {
+    resetUser();
+    cookies.remove("token");
+  };
   return (
     <header className={styles["bg-blue"]}>
       <div className={styles["header-menu"]}>
-        <a className={styles["header-logo"]} href="">
+        <Link className={styles["header-logo"]} to="/">
           <img src="/image/img_logo.png" alt="로고" />
-        </a>
+        </Link>
         {/* <!-- 활성화 할 a태그에 on_active 클래스 추가 --> */}
         <a href="#">학생목록</a>
-        <a href="#" className={styles["on_active"]}>
+        <Link to="/viz" className={styles["on_active"]}>
           시각화
-        </a>
+        </Link>
         <a href="#">과제</a>
         <a href="#">설정</a>
       </div>
+
       <div>
-        <Link to="" className={styles["logout"]}>
+        {loggedInUserName === "" ? null : <a style={{ marginRight: "10px" }}>{loggedInUserName}님</a>}
+        <a onClick={logout} className={styles["logout"]}>
           <a>로그아웃</a>
-        </Link>
+        </a>
       </div>
     </header>
   );
