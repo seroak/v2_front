2a7a60d4458c5cdfc48f89e2aa83ffbcaa16c1ad
lee gyu yeol-mac <i1004gy@gmail.com>
2024-09-06 13:54:43 +0900

    [#136]feat: 새로고침시 get요청

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 import Visualization from "./pages/Visualization/Visualization";
 import Signup from "./pages/Signup/Signup";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
-import { useQuery } from "@tanstack/react-query";
+import Modify from "./pages/Modify/Modify";
+import { useUserStore } from "./store/user";
 import "./App.css";
 
 import { worker } from "./mocks/browser";
+
 interface User {
+  email: string;
   name: string;
+  role: string;
 }
 //zustand
-import { useUserStore } from "./store/user";
+
 if (typeof window !== "undefined") {
   if (import.meta.env.VITE_APP_NODE_ENV === "development") {
     worker.start();
@@ -20,7 +25,7 @@ if (typeof window !== "undefined") {
 }
 // msw 버그로 인해 fetchUser는 따로 확인 불가능 백엔드 서버 연결해서 확인해야함
 const fetchUser = async (): Promise<User> => {
-  const response = await fetch("htttp://localhost:8083/edupi_user/v1/member/load", {
+  const response = await fetch("http://localhost:8080/edupi_user/v1/member/login/info", {
     method: "GET",
     credentials: "include",
     headers: {
@@ -30,19 +35,23 @@ const fetchUser = async (): Promise<User> => {
   if (!response.ok) {
     throw new Error("Network response was not ok");
   }
-  return await response.json();
+  const data = await response.json();
+  return data;
 };
 
 function App() {
-  const loggedInUserName = useUserStore((state) => state.setLoggedInUserName);
-  const { data } = useQuery<User, Error>({ queryKey: ["user"], queryFn: fetchUser });
+  const { data } = useQuery<User>({ queryKey: ["user"], queryFn: fetchUser });
+  const setLoggedInUserEmail = useUserStore((state) => state.setLoggedInUserEmail);
+  const setLoggedInUserName = useUserStore((state) => state.setLoggedInUserName);
+  const setLoggedInUserRole = useUserStore((state) => state.setLoggedInUserRole);
 
   useEffect(() => {
     if (data) {
-      loggedInUserName(data.name);
+      setLoggedInUserEmail(data.email);
+      setLoggedInUserName(data.name);
+      setLoggedInUserRole(data.role);
     }
   }, [data]);
-
   return (
     <Router>
       <Routes>
@@ -50,6 +59,7 @@ function App() {
         <Route path="/viz" element={<Visualization />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/modify" element={<Modify />} />
       </Routes>
     </Router>
   );
