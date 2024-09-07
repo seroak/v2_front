ea9d5131b32f96962735bf33e99e891677a5c279
lee gyu yeol-mac <i1004gy@gmail.com>
2024-09-07 21:40:11 +0900

    [#140]feat: 그룹페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Signup from "./pages/Signup/Signup";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import Modify from "./pages/Modify/Modify";
+import Group from "./pages/Group/Group";
 import { useUserStore } from "./store/user";
 import "./App.css";
 
@@ -69,6 +70,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/modify" element={<Modify />} />
+        <Route path="/group" element={<Group />} />
       </Routes>
     </Router>
   );
