abdebef29c12a6f59ac7d06eac08ca82ece530ac
lee gyu yeol-mac <i1004gy@gmail.com>
2024-09-13 15:16:59 +0900

    [#149]feat: 메인 페이지

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <>
       {loggedInUserName === "" ? <PublicHeader /> : <LoggedInHeader />}
-      <h1>s3 퍼블릭 차단</h1>
+      <h1>메인 페이지입니다</h1>
     </>
   );
 };
