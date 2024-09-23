35e9dfbff6b347b43a5c5ca4703576662ac13331
lee gyu yeol-mac <i1004gy@gmail.com>
2024-09-24 00:45:50 +0900

    [#152]feat: 이메일인증 리다이렉트 페이지

--- src/pages/AuthEmail/AuthEmail.tsx
+++ src/pages/AuthEmail/AuthEmail.tsx
@@ -1,16 +1,13 @@
 import { useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const AuthEmail = () => {
+  const [searchParams, _] = useSearchParams();
   const navigate = useNavigate();
-  const location = useLocation();
-
   useEffect(() => {
     const fetchData = async () => {
       // 쿼리 스트링에서 값을 추출
-      const queryParams = new URLSearchParams(location.search);
-      const email = queryParams.get("email"); // 예시: ?email=user@example.com
-
+      const email = searchParams.get("email"); // 예시: ?
       if (!email) {
         console.error("No email found in query string");
         return;
@@ -18,19 +15,28 @@ const AuthEmail = () => {
 
       try {
         // 쿼리 스트링 값을 fetch 요청에 포함
-        const response = await fetch(`http://localhost:4000/api/data?email=${email}`);
-        const result = await response.json();
-        console.log(result);
+        const response = await fetch(`http://localhost:8080/edupi_user/v1/member/activate`, {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email: email }),
+        });
+        /*
+         * TODO: 백앤드 코드가 변경되어 json응답이 오면아래 코드를 활성화합니다.
+         */
+        // const result = await response.json();
 
         // 데이터가 성공적으로 받아졌다면 리다이렉트
-        navigate("/login", { state: { data: result } });
+        navigate("/login");
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
-  }, [location, navigate]); // location과 navigate 의존성 추가
+  }, []); // location과 navigate 의존성 추가
 
   return null;
 };
