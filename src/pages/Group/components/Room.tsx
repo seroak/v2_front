104df09b923f41a14dcef33cc782b8bda047e696
lee gyu yeol-mac <i1004gy@gmail.com>
2024-09-25 15:27:14 +0900

    [#154]feat: class 이름 변경되게 수정

--- src/pages/Group/components/Room.tsx
+++ src/pages/Group/components/Room.tsx
@@ -1,5 +1,6 @@
 interface ClassData {
   id: number;
+  name: string;
   totalPeople: number;
 }
 interface props {
@@ -10,7 +11,9 @@ const Room = ({ classData }: props) => {
     <li>
       <a href={`http://localhost:8080/group/progress/${classData.id}`}>
         <div className="data04-name">
-          <p>파이썬 기초 {classData.id}반</p>
+          <p>
+            {classData.name} {classData.id}반
+          </p>
           <span>인원 : {classData.totalPeople}명</span>
           <span>초대 대기 : 1명</span>
         </div>
