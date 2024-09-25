2446bdf9fbb12d424446c3329feaabb847d2f232
lee gyu yeol-mac <i1004gy@gmail.com>
2024-09-25 15:10:49 +0900

    [#154]feat: 클래스 생성 api 추가

--- src/pages/Group/Group.tsx
+++ src/pages/Group/Group.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import LoggedInHeader from "../components/LoggedInHeader";
 import Room from "./components/Room";
 import { useMswReadyStore } from "@/store/mswReady";
 import { useUserStore } from "@/store/user";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import { getGroup } from "@/services/api";
 interface Classroom {
   id: number;
@@ -22,7 +22,7 @@ interface GroupData {
 const Group = () => {
   const isMswReady = useMswReadyStore((state) => state.isMswReady);
   const LoggedInUserName = useUserStore((state) => state.loggedInUserName);
-  const { data } = useQuery<GroupData>({
+  const { data, refetch } = useQuery<GroupData>({
     queryKey: ["group"],
     queryFn: getGroup,
     enabled: isMswReady,
@@ -30,12 +30,40 @@ const Group = () => {
 
   const [hostClassRooms, setHostClassRooms] = useState<Classroom[]>([]);
   const [guestClassRooms, setGuestClassRooms] = useState<Classroom[]>([]);
+  const [createClassName, setCreateCalssName] = useState<string | undefined>();
   useEffect(() => {
     if (data) {
       setHostClassRooms(data.host.classrooms);
       setGuestClassRooms(data.guest.classrooms);
     }
   }, [data]);
+  const changeCreateClassName = (e: ChangeEvent<HTMLInputElement>) => {
+    setCreateCalssName(e.target.value);
+  };
+  const submitCreateClassName = () => {
+    if (createClassName) {
+      mutation.mutate(createClassName);
+    } else {
+      console.error("클래스 이름을 입력하세요");
+    }
+  };
+  const mutation = useMutation({
+    mutationFn: async (createClass: string) => {
+      return fetch("http://localhost:8080/edupi-lms/v1/classroom", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name: createClass }),
+      });
+    },
+    async onSuccess(data) {
+      const jsonData = await data.json();
+      console.log(jsonData); //성공 로그
+      refetch();
+    },
+    onError(error) {
+      console.error("클래스 생성 에러", error);
+    },
+  });
   return (
     <div className="bg2" style={{ minWidth: "1521px" }}>
       <LoggedInHeader />
@@ -66,8 +94,14 @@ const Group = () => {
             </ul>
             <label htmlFor="addgroup">그룹 생성</label>
             <div>
-              <input type="text" id="addgroup" placeholder="그룹 이름" />
-              <button>생성</button>
+              <input
+                type="text"
+                id="addgroup"
+                placeholder="그룹 이름"
+                value={createClassName}
+                onChange={changeCreateClassName} // 입력값 변경시 상태 업데이트
+              />
+              <button onClick={submitCreateClassName}>생성</button>
             </div>
           </div>
         </div>
