781e3ccb305f1e1aa561bc63afd5db7e4958d422
lee gyu yeol-mac <i1004gy@gmail.com>
2024-09-25 14:00:45 +0900

    [#154]feat: api수정 mswready수정

--- src/services/api.ts
+++ src/services/api.ts
@@ -19,3 +19,22 @@ export const login = (req: LoginUser) =>
     headers: { "Content-Type": "application/json" },
     withCredentials: true,
   });
+export const getGroup = async () => {
+  try {
+    const response = await fetch("http://localhost:8080/edupi-lms/v1/classroom", {
+      method: "GET",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("An error occurred:", error);
+    throw error;
+  }
+};
