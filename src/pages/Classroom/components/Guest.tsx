b191103f02579e2f9abc4efd7457c9807a1fa5b8
lee gyu yeol-mac <i1004gy@gmail.com>
2024-09-26 17:00:09 +0900

    [#158]feat: 학생 정보 보여주는 컴포넌트 데이터 보여지게 수정

--- src/pages/Classroom/components/Guest.tsx
+++ src/pages/Classroom/components/Guest.tsx
@@ -1,14 +1,35 @@
-const Guest = () => {
+interface GuestType {
+  guestId: number;
+  name: string;
+  status: number | string;
+}
+interface props {
+  guest: GuestType;
+}
+const Guest = ({ guest }: props) => {
+  switch (guest.status) {
+    case 0:
+      guest.status = "제출 중";
+      break;
+    case 1:
+      guest.status = "제출 완료";
+      break;
+    case 2:
+      guest.status = "도움";
+      break;
+    default:
+      null;
+  }
   return (
     <>
       <li>
         <a href="#">
           <div>
-            <p>홍길동</p>
+            <p>{guest.name}</p>
             <span>제출시간 14:02</span>
           </div>
           <div className="progress-success">
-            <p>성공</p>
+            <p>{guest.status}</p>
           </div>
         </a>
       </li>
