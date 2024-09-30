a5a772634faba42b6a3af392f7d284eac882e0e4
lee gyu yeol-mac <i1004gy@gmail.com>
2024-09-30 21:52:43 +0900

    [#158]feat: guset 컴포넌트 분리

---
+++ src/pages/Classroom/components/Guest.tsx
@@ -0,0 +1,18 @@
+const Guest = () => {
+  return (
+    <>
+      <li>
+        <a href="#">
+          <div>
+            <p>홍길동</p>
+            <span>제출시간 14:02</span>
+          </div>
+          <div className="progress-success">
+            <p>성공</p>
+          </div>
+        </a>
+      </li>
+    </>
+  );
+};
+export default Guest;
