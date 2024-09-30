cd6022fa83e769e9c258fa2169a486931554dfba
lee gyu yeol-mac <i1004gy@gmail.com>
2024-09-30 16:05:35 +0900

    [#158]feat: 수정 화면 api 추가

--- src/pages/Modify/Modify.tsx
+++ src/pages/Modify/Modify.tsx
@@ -1,8 +1,93 @@
-import LoggedInHeader from "../components/LoggedInHeader";
+import LoggedInClassroomHeader from "@/pages/components/LoggedInClassroomHeader";
+import Guest from "./components/Guest";
+import { useMswReadyStore } from "@/store/mswReady";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+interface GuestType {
+  guestId: number;
+  email: string;
+  name: string;
+  status: number;
+}
+interface TotalInfoType {
+  ing: number;
+  complete: number;
+  help: number;
+}
+
+interface ClassroomData {
+  result: {
+    className: string;
+    totalInfo: TotalInfoType;
+    guest: GuestType[];
+  };
+}
+
 const Modify = () => {
+  const params = useParams();
+  const classroomId = params.classroomId;
+  const [guests, setGuests] = useState<GuestType[]>();
+  const isMswReady = useMswReadyStore((state) => state.isMswReady);
+  const inviteClassroom = async (classroomId: string) => {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/edupi-lms/v1/classroom/account/invite?clssroomId=${classroomId}`,
+        {
+          method: "POST",
+          credentials: "include",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("An error occurred:", error);
+      throw error;
+    }
+  };
+  const getClassroomData = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/edupi-lms/v1/classroom/account/guest?clssroomId=${classroomId}`,
+        {
+          method: "GET",
+          credentials: "include",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("An error occurred:", error);
+      throw error;
+    }
+  };
+  const { data, refetch: getClassroomRefetch } = useQuery<ClassroomData>({
+    queryKey: ["classroomData"],
+    queryFn: getClassroomData,
+    enabled: isMswReady,
+  });
+
+  console.log(classroomId);
+  useEffect(() => {
+    console.log(data);
+    if (data) {
+      setGuests(data.result.guest);
+    }
+  }, [data]);
+
   return (
     <div className="bg">
-      <LoggedInHeader />
+      <LoggedInClassroomHeader classroom={classroomId} />
       <div id="header02" className="bg-blue"></div>
       <div className="group-wrap">
         <div className="group-left">
@@ -10,47 +95,17 @@ const Modify = () => {
           <h2 className="group-title">파이썬 기초 1반</h2>
         </div>
         <div className="group-link">
-          <p className="link-name">
-            <img src="/image/icon_link.svg" alt="링크" />
-            그룹 초대 링크
-          </p>
-          <p className="link-data">https://www.google.com/webhp?hl=ko&sa=</p>
-          <button>
-            <img src="/image/icon_copy.svg" alt="복사하기" />
-          </button>
+          <p className="link-name">클래스룸 초대</p>
+          <div className="invite-wrap">
+            <input className="invite-input"></input>
+            <button className="invite-button">
+              <p>초대하기</p>
+            </button>
+          </div>
         </div>
       </div>
       <div className="s__container">
         <div className="modify-s__row">
-          <div className="section-title">
-            <div className="title-left">
-              <h3>초대 대기</h3>
-            </div>
-          </div>
-          <ul className="section-data section-data03">
-            <li>
-              <div className="data03-name">
-                <p>김민준</p>
-                <span>minjun@naver.com</span>
-                <span>학번 : 20003204</span>
-              </div>
-              <div className="data03-btns">
-                <button className="blue">추가</button>
-                <button className="red">거절</button>
-              </div>
-            </li>
-            <li>
-              <div className="data03-name">
-                <p>이서준</p>
-                <span>seojun@naver.com</span>
-                <span>학번 : 20003204</span>
-              </div>
-              <div className="data03-btns">
-                <button className="blue">추가</button>
-                <button className="red">거절</button>
-              </div>
-            </li>
-          </ul>
           <div className="section-title">
             <div className="title-left">
               <h3>가입된 학생</h3>
@@ -65,51 +120,14 @@ const Modify = () => {
             </div>
           </div>
           <ul className="section-data section-data03">
-            <li>
-              <div className="data03-name">
-                <p>이서윤</p>
-                <span>seoyun@naver.com</span>
-                <span>학번 : 20003204</span>
-              </div>
-              <div className="data03-btns">
-                <button className="red">방출</button>
-              </div>
-            </li>
-            <li>
-              <div className="data03-name">
-                <p>이서연</p>
-                <span>seoyeon@naver.com</span>
-                <span>학번 : 20003204</span>
-              </div>
-              <div className="data03-btns">
-                <button className="red">방출</button>
-              </div>
-            </li>
-            <li>
-              <div className="data03-name">
-                <p>김지우</p>
-                <span>jiwo@naver.com</span>
-                <span>학번 : 20003204</span>
-              </div>
-              <div className="data03-btns">
-                <button className="red">방출</button>
-              </div>
-            </li>
-            <li>
-              <div className="data03-name">
-                <p>박예준</p>
-                <span>yeajun@naver.com</span>
-                <span>학번 : 20003204</span>
-              </div>
-              <div className="data03-btns">
-                <button className="red">방출</button>
-              </div>
-            </li>
+            {guests?.map((guest) => (
+              <Guest key={guest.guestId} guest={guest} />
+            ))}
           </ul>
           <div className="right-btns">
             <button className="red">
               <img src="/image/icon_delete.svg" alt="그룹삭제" />
-              그룹삭제
+              클래스룸 삭제
             </button>
           </div>
         </div>
