b254e144f300755700709808b718d2a48dce2dc4
lee gyu yeol-mac <i1004gy@gmail.com>
2024-10-01 18:14:13 +0900

    [#161]feat: 방출 api 작성

--- src/pages/Manage/components/Guest.tsx
+++ src/pages/Manage/components/Guest.tsx
@@ -1,5 +1,6 @@
+import { useMutation } from "@tanstack/react-query";
 interface GuestType {
-  guestId: number;
+  id: number;
   name: string;
   email: string;
   status: number | string;
@@ -7,7 +8,42 @@ interface GuestType {
 interface props {
   guest: GuestType;
 }
+const fetchEmissionGuest = async (classroomAccountId: number) => {
+  try {
+    const response = await fetch(`http://localhost:8080/edupi-lms/v1/classroom/account/${classroomAccountId}`, {
+      method: "DELETE",
+      credentials: "include",
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("An error occurred:", error);
+    throw error;
+  }
+};
+
 const Guest = ({ guest }: props) => {
+  const useEmissoionGuestMutation = () => {
+    return useMutation({
+      mutationFn: fetchEmissionGuest,
+      onSuccess: () => {
+        alert("방출되었습니다.");
+      },
+      onError: (error) => {
+        console.error("An error occurred:", error);
+        alert("방출에 실패했습니다.");
+      },
+    });
+  };
+  const emissionGuestMutation = useEmissoionGuestMutation();
+  console.log(guest);
+  const id = Number(guest.id);
+  const handleEmissionGuest = () => {
+    emissionGuestMutation.mutate(id);
+  };
   return (
     <li>
       <div className="data03-name">
@@ -15,7 +51,9 @@ const Guest = ({ guest }: props) => {
         <span>{guest.email}</span>
       </div>
       <div className="data03-btns">
-        <button className="red">방출</button>
+        <button className="red" onClick={handleEmissionGuest}>
+          방출
+        </button>
       </div>
     </li>
   );
