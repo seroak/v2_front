8cf47a0522f0dc57f176d731a212cfd6b6a69980
lee gyu yeol-mac <i1004gy@gmail.com>
2024-10-01 15:07:32 +0900

    [#161]feat: 클래스룸 용 시각화 페이지 작성

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import Visualization from "./pages/Visualization/Visualization";
+import VisualizationClassroom from "./pages/Visualization/VisualizationClassroom";
 import Signup from "./pages/Signup/Signup";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import Manage from "./pages/Manage/Manage";
-import ClassroomSpace from "./pages/ClassroomSpace/ClassRoomSpace";
+import ClassroomSpace from "./pages/ClassroomSpace/ClassroomSpace";
 import Assginment from "./pages/Assignment/Assignment";
 import Clssroom from "./pages/Classroom/Classroom";
 import AuthEmail from "./pages/AuthEmail/AuthEmail";
@@ -53,6 +54,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/viz" element={<Visualization />} />
+        <Route path="/classroomspace/classroom/viz/:classroomId" element={<VisualizationClassroom />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/classroomspace/classroom/manage/:classroomId" element={<Manage />} />
