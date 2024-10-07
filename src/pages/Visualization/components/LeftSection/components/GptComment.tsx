8334b6fa923025f2d7f20a48899c8fe0142eb090
lee gyu yeol-mac <i1004gy@gmail.com>
2024-10-07 23:34:18 +0900

    [#159]feat: gpt 부분 pending success 부분 분기 처리

--- src/pages/Visualization/components/LeftSection/components/GptComment.tsx
+++ src/pages/Visualization/components/LeftSection/components/GptComment.tsx
@@ -1,13 +1,19 @@
 import { useMutation } from "@tanstack/react-query";
-import { useContext, useRef } from "react";
+import { useContext, useRef, useState } from "react";
 import { CodeContext } from "@/pages/Visualization/Visualization";
 import { useTimeoutStore } from "@/store/timeout";
 import { useGptTooltipStore } from "@/store/gptTooltip";
+interface ModifidCode {
+  line: number;
+  code: string;
+}
 const GptComment = () => {
   const context = useContext(CodeContext);
   const { setTimeoutId, clearCurrentTimeout } = useTimeoutStore();
   const { setIsGptToggle, isGptToggle } = useGptTooltipStore();
   const { gptLeft, gptTop } = useGptTooltipStore();
+  const [reason, setReason] = useState<string>("");
+  const [modifiedCode, setModifiedCode] = useState<ModifidCode[]>([]);
   const timeoutRef = useRef<number | null>(null);
   if (!context) {
     console.error("CodeContext not found");
@@ -44,6 +50,8 @@ const GptComment = () => {
     mutationFn: gptCorrect,
     async onSuccess(data) {
       console.log("data", data);
+      setReason(data.result.reason);
+      setModifiedCode(data.result.modified_codes);
     },
     onError(error) {
       console.error("An error occurred:", error);
@@ -62,8 +70,38 @@ const GptComment = () => {
       onMouseLeave={handleMouseLeave}
     >
       <img src="/image/icon_gpt.svg" alt="gpt" />
-      <button onClick={handleCorrect}>즉시교정</button>
-      <button>힌트보기</button>
+
+      {mutation.isSuccess ? (
+        <div>
+          이유: {reason}
+          <br />
+          <div className="code-container">
+            <div className="line-numbers">
+              {modifiedCode.map((code, index) => (
+                <div key={index}>
+                  <span>{code.line}</span>
+                </div>
+              ))}
+            </div>
+            <div className="code-content">
+              {modifiedCode.map((code, index) => (
+                <div key={index}>
+                  <span>{code.code}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      ) : mutation.isPending ? (
+        <div className="spinner-container">
+          <div className="spinner"></div>
+        </div>
+      ) : (
+        <>
+          <button onClick={handleCorrect}>즉시교정</button>
+          <button>힌트보기</button>
+        </>
+      )}
     </div>
   );
 };
