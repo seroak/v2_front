29b401d7b224771877351628e608dd9716446d7a
lee gyu yeol-mac <i1004gy@gmail.com>
2024-10-10 14:08:42 +0900

    [#159]feat힌트보기 즉시 수정 api 연결

--- src/pages/Visualization/components/LeftSection/components/GptComment.tsx
+++ src/pages/Visualization/components/LeftSection/components/GptComment.tsx
@@ -3,6 +3,7 @@ import { useContext, useRef, useState } from "react";
 import { CodeContext } from "@/pages/Visualization/Visualization";
 import { useTimeoutStore } from "@/store/timeout";
 import { useGptTooltipStore } from "@/store/gptTooltip";
+import { useEditorStore } from "@/store/editor";
 interface ModifidCode {
   line: number;
   code: string;
@@ -10,7 +11,12 @@ interface ModifidCode {
 const GptComment = () => {
   const context = useContext(CodeContext);
   const { setTimeoutId, clearCurrentTimeout } = useTimeoutStore();
-  const { setIsGptToggle, isGptToggle } = useGptTooltipStore();
+  const { setIsGptToggle } = useGptTooltipStore();
+  const { resetEditor } = useEditorStore();
+  const gptPin = useGptTooltipStore((state) => state.gptPin);
+  const setGptPin = useGptTooltipStore((state) => state.setGptPin);
+  const [hint, setHint] = useState<string>("");
+  const [hintLine, setHintLine] = useState<number>(0);
   const { gptLeft, gptTop } = useGptTooltipStore();
   const [reason, setReason] = useState<string>("");
   const [modifiedCode, setModifiedCode] = useState<ModifidCode[]>([]);
@@ -27,13 +33,16 @@ const GptComment = () => {
   };
   const handleMouseLeave = () => {
     clearCurrentTimeout();
-    timeoutRef.current = setTimeout(() => {
-      setIsGptToggle(false);
-      setTimeoutId(null);
-    }, 300);
-    setTimeoutId(timeoutRef.current);
+
+    if (!gptPin) {
+      timeoutRef.current = setTimeout(() => {
+        setIsGptToggle(false);
+        setTimeoutId(null);
+      }, 300);
+      setTimeoutId(timeoutRef.current);
+    }
   };
-  const gptCorrect = async () => {
+  const fetchGptCorrect = async (code: any) => {
     const response = await fetch("http://localhost:8080/edupi-syntax/v1/advice/correct", {
       method: "POST",
       credentials: "include",
@@ -46,8 +55,8 @@ const GptComment = () => {
     return response.json();
   };
 
-  const mutation = useMutation({
-    mutationFn: gptCorrect,
+  const gptCorrectMutation = useMutation({
+    mutationFn: fetchGptCorrect,
     async onSuccess(data) {
       setReason(data.result.reason);
       const newModifiedCode = data.result.modified_codes.reduce((acc: ModifidCode[], cur: ModifidCode) => {
@@ -69,12 +78,65 @@ const GptComment = () => {
       console.error("An error occurred:", error);
     },
   });
-  const handleAprrove = () => {};
+  const fetchGptHint = async (code: any) => {
+    const response = await fetch("http://localhost:8080/edupi-syntax/v1/advice/hint", {
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ source_code: code }),
+    });
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  };
+  const gptHintMutation = useMutation({
+    mutationFn: fetchGptHint,
+    async onSuccess(data) {
+      setHint(data.result.hint);
+      setHintLine(data.result.line);
+    },
+    onError(error) {
+      console.error("An error occurred:", error);
+    },
+  });
+  const handleAprrove = () => {
+    const newCode = code
+      .split("\n")
+      .map((line, index) => {
+        if (modifiedCode[index] && modifiedCode[index].line === index + 1) {
+          return modifiedCode[index].code;
+        }
+        return line;
+      })
+      .join("\n");
+    setCode(newCode);
+    setGptPin(false);
+    setIsGptToggle(false);
+  };
 
-  const handleReject = () => {};
+  // 창을 닫는 버튼
+  const handleReject = () => {
+    clearCurrentTimeout();
+    setIsGptToggle(false);
+    setGptPin(false);
+  };
 
+  // 즉시 교정 버튼
   const handleCorrect = async () => {
-    mutation.mutate();
+    setGptPin(true);
+    clearCurrentTimeout();
+    setIsGptToggle(true);
+    resetEditor();
+    gptCorrectMutation.mutate(code);
+  };
+
+  // 힌트보기 버튼
+  const handleHint = async () => {
+    setGptPin(true);
+    clearCurrentTimeout();
+    setIsGptToggle(true);
+    gptHintMutation.mutate(code);
   };
 
   return (
@@ -86,12 +148,12 @@ const GptComment = () => {
     >
       <img src="/image/icon_gpt.svg" alt="gpt" />
 
-      {mutation.isSuccess ? (
+      {gptCorrectMutation.isSuccess ? (
         <div className="gpt-success">
           이유: {reason}
           <br />
           <div className="code-container">
-            <div className="line-numbers">
+            <div className="gpt-line-numbers">
               {modifiedCode.map((code, index) => (
                 <div key={index}>
                   {code.line === 0 ? (
@@ -129,14 +191,24 @@ const GptComment = () => {
             </button>
           </div>
         </div>
-      ) : mutation.isPending ? (
+      ) : gptHintMutation.isSuccess ? (
+        <div className="gpt-hint">
+          <div>힌트 라인: {hintLine}</div>
+          <div>힌트: {hint}</div>
+          <div className="button-left">
+            <button className="approve" onClick={handleAprrove}>
+              수락
+            </button>
+          </div>
+        </div>
+      ) : gptCorrectMutation.isPending || gptHintMutation.isPending ? (
         <div className="spinner-container">
           <div className="spinner"></div>
         </div>
       ) : (
         <>
           <button onClick={handleCorrect}>즉시교정</button>
-          <button>힌트보기</button>
+          <button onClick={handleHint}>힌트보기</button>
         </>
       )}
     </div>
