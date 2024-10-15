90db7e5eb2900624d28b2500d0dbda58687003b7
lee gyu yeol-mac <i1004gy@gmail.com>
2024-10-15 14:36:32 +0900

    [#167]feat: 함수 호출 mock데이터

--- src/mocks/samples/UDFResPiece.ts
+++ src/mocks/samples/UDFResPiece.ts
@@ -13,6 +13,16 @@ const UDFResPiece = [
     callStack: "main",
     type: "assign",
   },
+  //2. 함수 호출 부분
+  {
+    // 왼쪽 코드흐름 - 함수 만드는 부분
+    id: 1, // 호출하는 라인 번호
+    assignName: "result", // 함숭리턴값을 할당하는 변수, 없으면 ""
+    signature: "isEven(name)",
+    depth: 2,
+    code: "result=isEven(name)",
+    type: "callUserFunc",
+  },
 ];
 
 export default UDFResPiece;
