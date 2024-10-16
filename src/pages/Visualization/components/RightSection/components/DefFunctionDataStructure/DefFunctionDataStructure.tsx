65463a8c9c1aa1b107ec4a47a03bc1c1f2df84ad
lee gyu yeol-mac <i1004gy@gmail.com>
2024-10-16 19:38:42 +0900

    [#178]feat: 화살표 해결

--- src/pages/Visualization/components/RightSection/components/DefFunctionDataStructure/DefFunctionDataStructure.tsx
+++ src/pages/Visualization/components/RightSection/components/DefFunctionDataStructure/DefFunctionDataStructure.tsx
@@ -1,6 +1,6 @@
 import styles from "./DefFunctionDataStructure.module.css";
 import { DataStructureFunctionItem } from "@/pages/Visualization/types/dataStructuresItem/dataStructureFunctionItem";
-import cx from "classnames";
+
 type Props = {
   functionItem: DataStructureFunctionItem;
 };
@@ -8,18 +8,18 @@ const DefFunctionDataStructure = ({ functionItem }: Props) => {
   const { expr, isLight } = functionItem;
 
   return (
-      <div className={styles["container"]}>
-          <div className={styles["left-container"]}>
-              <span className={styles["def"]}>def</span>
-              <img src="/image/img_function_arrow.svg"/>
-          </div>
-          <div className={styles["right-container"]}>
-              <span className={styles["func"]}>func</span>
-              <div className={styles["func-data"]}>
-                  <span>{expr}</span>
-              </div>
-          </div>
+    <div className={styles["container"]}>
+      <div className={styles["left-container"]}>
+        <span className={styles["def"]}>def</span>
+        <img src="/image/img_function_arrow.svg" />
       </div>
+      <div className={styles["right-container"]}>
+        <span className={styles["func"]}>func</span>
+        <div className={styles["func-data"]}>
+          <span>{expr}</span>
+        </div>
+      </div>
+    </div>
   );
 };
 
