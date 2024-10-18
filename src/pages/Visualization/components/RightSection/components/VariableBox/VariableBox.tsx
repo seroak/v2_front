8fc2326741ced05d5136c5122718babe4a73f6f4
lee gyu yeol-mac <i1004gy@gmail.com>
2024-10-18 18:53:32 +0900

    [#182]feat: 타입 정리

--- src/pages/Visualization/components/RightSection/components/VariableBox/VariableBox.tsx
+++ src/pages/Visualization/components/RightSection/components/VariableBox/VariableBox.tsx
@@ -3,17 +3,17 @@ import cx from "classnames";
 type Props = {
   value: string;
   name: string;
-  isLight: boolean;
+  isLight?: boolean;
 };
 function VariableBox({ value, name, isLight }: Props) {
   return (
     <li>
-        <div className="var-container">
-             <div className="name">{name}</div>
-             <div className={cx(styles["var-data"], isLight && styles.highlight)}>
-                <span>{value}</span>
-             </div>
+      <div className="var-container">
+        <div className="name">{name}</div>
+        <div className={cx(styles["var-data"], isLight && styles.highlight)}>
+          <span>{value}</span>
         </div>
+      </div>
     </li>
   );
 }
