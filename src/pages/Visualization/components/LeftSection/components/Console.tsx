768e9c4f64cce7802044736cef9fda562b13179d
lee gyu yeol-mac <i1004gy@gmail.com>
2024-11-11 15:55:22 +0900

    [#236]feat: 콘솔창에 input 창 추가

--- src/pages/Visualization/components/LeftSection/components/Console.tsx
+++ src/pages/Visualization/components/LeftSection/components/Console.tsx
@@ -4,11 +4,24 @@ import styles from "./Console.module.css";
 const Console = () => {
   const consoleText = useConsoleStore((state) => state.console);
   const stepIdx = useConsoleStore((state) => state.stepIdx);
+  const { inputData, setInputData } = useConsoleStore();
+  const handleConsoleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInputData(e.target.value);
+
+    e.target.style.height = "auto";
+    e.target.style.height = `${e.target.scrollHeight}px`;
+  };
 
   return (
     <div className={styles["view-section1-2"]}>
       <p className={styles["view-section-title"]}>Console</p>
       <div className={styles["view-data"]}>
+        <textarea
+          className={styles["input-area"]}
+          value={inputData}
+          onChange={(e) => handleConsoleTextChange(e)}
+          placeholder="Enter text here..."
+        />
         <div className={styles.consoleText}>{consoleText[stepIdx]}</div>
       </div>
     </div>
